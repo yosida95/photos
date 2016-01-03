4b43d7194a74a7130e1c07f513e830a09ad9ab9a
yosida95 <kohei@yosida95.com>
2016-01-04 04:41:12 +0900

    update install_requires

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ from setuptools import (
 
 
 requires = [
-    'pyramid',
+    'pyramid>=1.6,<1.7',
     'pyramid_tm',
-    'pyramid_jinja2',
-    'SQLAlchemy',
+    'SQLAlchemy>=1.0,<1.1',
     'transaction',
     'zope.sqlalchemy',
+
+    'boto>=2.38,<2.39',
+    'Pillow>=3.0,<3.1',
+    'PyMySQL>=0.6,<0.7',
+    'pyramid_jinja2>=2.5,<2.6',
 ]
 
 setup(name='photos',
